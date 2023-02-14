0519970a73a3b7825062aec90cd9a1fcbd7b7563
Aubrey <116300259+Br3y@users.noreply.github.com>
2023-02-15 07:53:33 +0800

    restarting myself and cleaning code

--- README.md
+++ README.md
@@ -1,14 +1,1 @@
 # rock-paper-scissors
-
-
-computer gets to pick a random 
-
-players gets to pick
-
-comparing player to computer
-
-if
-    computer and players picks the same
-        the they are draw
-if
-    computer picks rock and 
--- javascript.js
+++ javascript.js
@@ -1,22 +1,23 @@
 //Randomly retrn either rock paper or scissors
 function getComputerChoice(){
     let computerChoice = ["rock", "paper", "scissocrs"];
-    ;
-    return computerChoice[Math.floor(Math.random(computerChoice) * 3) + 1]; 
+    return computerChoice[Math.floor(Math.random(computerChoice) * 3)]; 
 }
 
 function singleRound(playerSelection, computerSelection){
-    playerSelection.toLowerCase();
-
 
 }
 
 const playerSelection = prompt("Whats ur choice?");
 const computerSelection = getComputerChoice();
-console.log(computerSelection)
+// console.log(computerSelection)
 console.log(playRound(playerSelection, computerSelection));
 
 function game(){
     //call the playround funcction inside of this one to play a 5
     // round game that keeps score and reports a winner or loser at the end
+    for (let i = 0; i < 5; i ++){
+        
+    }
+
 }

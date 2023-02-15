5fb9fb587b0dd866c19f35812b9f325c68bebc43
Aubrey <116300259+Br3y@users.noreply.github.com>
2023-02-15 09:09:27 +0800

    adding game() function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,11 @@
-//Randomly retrn either rock paper or scissors
+let playerScore = 0;
+let computerScore = 0;
+
+//Randomly return either rock paper or scissors
 function getComputerChoice(){
+    
     let computerChoice = ["rock", "paper", "scissors"];
+    // computer random pick from 0 - rock 1 - paper 2 - scissors
     return computerChoice[Math.floor(Math.random(computerChoice) * 3)]; 
 }
 
@@ -8,46 +13,64 @@ function playRound(playerSelection, computerSelection){
     // Lowercase all playerSelection to make it case insensitive
     playerSelection = playerSelection.toLowerCase();
 
+    // console playerSelection
     console.log(`players pick ${playerSelection}`)
-    
+    // Rock Paper Scissors comparing who will win
     if (playerSelection === computerSelection){
         console.log("It's a tie");
     }
     else if (playerSelection === "rock" && computerSelection === "paper"){
         console.log("You Lose! Paper beats Rock");
+        computerScore += 1;
     }
     else if (playerSelection === "rock" && computerSelection === "scissors"){
         console.log("You Win! Rock beats Scissors")
+        playerScore += 1;
     }
     else if (playerSelection === "paper" && computerSelection === "rock"){
         console.log("You Win! Paper beats Rock")
+        playerScore += 1;
     }
     else if (playerSelection === "paper" && computerSelection === "scissors"){
         console.log("You Lose! Scissors beats Paper")
+        computerScore += 1;
     }
     else if (playerSelection === "scissors" && computerSelection === "paper"){
         console.log("You Win! Scissors beats Paper")
+        playerScore += 1;
     }
     else if (playerSelection === "scissors" && computerSelection === "rock"){
         console.log("You Lose! Rock beats Scissors")
+        computerScore += 1;
     }
 
 }
 
+// Declare playerSelection
 const playerSelection = prompt("Whats ur choice? Rock/Paper/Scissors");
+// Declare computerSelection
 const computerSelection = getComputerChoice();
 // console computerSelection
 console.log(`computer picks ${computerSelection}`)
-playRound(playerSelection, computerSelection)
 
+// Function Invocation
+// playRound(playerSelection, computerSelection)
 // // console.log(computerSelection)
 // console.log(playRound(playerSelection, computerSelection));
 
 function game(){
-    //call the playround funcction inside of this one to play a 5
-    // round game that keeps score and reports a winner or loser at the end
+    playRound(playerSelection, computerSelection)
     for (let i = 0; i < 5; i ++){
+        playRound(playerSelection, computerSelection)
+    }
 
+    if (computerScore > playerScore){
+        console.log("Computer won the game")
+    }
+    else if (playerScore > computerScore){
+        console.log("Player won the game");
+    } else {
+        console.log("It's a tie")
     }
 
 }

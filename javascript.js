7de9444a30799b96d19098a6b7e9bec3c50a8afe
Aubrey <116300259+Br3y@users.noreply.github.com>
2023-02-15 08:42:20 +0800

    finishing if else to playRound function w/ comments and adding console.log

--- javascript.js
+++ javascript.js
@@ -1,23 +1,53 @@
 //Randomly retrn either rock paper or scissors
 function getComputerChoice(){
-    let computerChoice = ["rock", "paper", "scissocrs"];
+    let computerChoice = ["rock", "paper", "scissors"];
     return computerChoice[Math.floor(Math.random(computerChoice) * 3)]; 
 }
 
-function singleRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection){
+    // Lowercase all playerSelection to make it case insensitive
+    playerSelection = playerSelection.toLowerCase();
+
+    console.log(`players pick ${playerSelection}`)
+    
+    if (playerSelection === computerSelection){
+        console.log("It's a tie");
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper"){
+        console.log("You Lose! Paper beats Rock");
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors"){
+        console.log("You Win! Rock beats Scissors")
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock"){
+        console.log("You Win! Paper beats Rock")
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors"){
+        console.log("You Lose! Scissors beats Paper")
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper"){
+        console.log("You Win! Scissors beats Paper")
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock"){
+        console.log("You Lose! Rock beats Scissors")
+    }
 
 }
 
-const playerSelection = prompt("Whats ur choice?");
+const playerSelection = prompt("Whats ur choice? Rock/Paper/Scissors");
 const computerSelection = getComputerChoice();
-// console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection));
+// console computerSelection
+console.log(`computer picks ${computerSelection}`)
+playRound(playerSelection, computerSelection)
+
+// // console.log(computerSelection)
+// console.log(playRound(playerSelection, computerSelection));
 
 function game(){
     //call the playround funcction inside of this one to play a 5
     // round game that keeps score and reports a winner or loser at the end
     for (let i = 0; i < 5; i ++){
-        
+
     }
 
 }

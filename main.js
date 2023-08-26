f01603a75e2e4c1cc179db1fbb05637e30e3e203
Aubrey Lizardo <116300259+Br3y@users.noreply.github.com>
2023-08-26 17:05:17 +0800

    create getComputerChoice that randomly return either rock, paper or scissor

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 const playerSelection = prompt("Enter pick? Rock, Paper, Scissor", '');
+const computerSelection = getComputerChoice();
+
+function getComputerChoice(){
+    const computerPick = Math.floor(Math.random() * 3) + 1;
+    if(computerPick === 1){
+        return 'Rock';
+    } else if (computerPick === 2){
+        return 'Paper';
+    } else if (computerPick === 3){
+        return 'Scissor';
+    }
+}
 

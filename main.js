a0b3a500fb116f9d48b84786d2919785cc7e3606
Aubrey Lizardo <116300259+Br3y@users.noreply.github.com>
2023-08-26 17:23:53 +0800

    create if statement if both pick are same, make it tie

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const computerSelection = getComputerChoice();
 
 function getComputerChoice(){
     const computerPick = Math.floor(Math.random() * 3) + 1;
+    console.log(computerPick)
     if(computerPick === 1){
         return 'Rock';
     } else if (computerPick === 2){
@@ -15,5 +16,8 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-    
+
+    if(playerSelection === computerSelection){
+        return `both are ${playerSelection}: TIE`;
+    }
 }

bf24505fee0a30de85d5adbd96e688f791323cfd
Aubrey Lizardo <116300259+Br3y@users.noreply.github.com>
2023-09-07 01:56:13 +0800

    creating const buttons using querySelectorAll

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let playerScore = 0;
 let computerScore = 0;
+const buttons = document.querySelectorAll("button");
+
 
 function getComputerChoice(){
     const computerPick = Math.floor(Math.random() * 3) + 1;

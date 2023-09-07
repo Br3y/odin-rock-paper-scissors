c866ea507ecf3f2f60d001ac4d3c5f04d9f3cf3a
Aubrey Lizardo <116300259+Br3y@users.noreply.github.com>
2023-09-07 14:38:22 +0800

    creating element and display player/computer total wins

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ let playerScore = 0;
 let computerScore = 0;
 const buttons = document.querySelectorAll("button");
 const result = document.querySelector("#result");
+const playerResult = document.createElement("div");
+const computerResult = document.createElement("div");
 
 buttons.forEach((button) => button.addEventListener("click", ()=>{
     let playerSelection = button.id;
     let computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
 }))
+
 function getComputerChoice(){
     const computerPick = ['rock', 'paper', 'scissor'];
-    return computerPick[Math.floor(Math.random() * 2) + 1];
+    return computerPick[Math.floor(Math.random() * 3) + 1];
 }
 
 function playRound(playerSelection, computerSelection){
@@ -41,6 +44,11 @@ function playRound(playerSelection, computerSelection){
         computerScore += 1;
         result.textContent =  'You lose! Scissor lose to Rock';
     }
+
+    playerResult.textContent = "Player Score: " + playerScore;
+    result.appendChild(playerResult)
+    computerResult.textContent = "Computer Score: " + computerScore;
+    result.appendChild(computerResult)
 
     if(computerScore === 5){
         result.textContent = 'You Win! Computer Wins, Reload to restart the game';
